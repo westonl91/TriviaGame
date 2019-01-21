f8f25ed28f9c4ca5ac557322b3928aed993a5073
Leah Weston <westonl91@yahoo.com>
2019-01-20 17:27:48 -0800

    no start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,20 @@
 //hw for section 5, Timers
 
-
-var time_left = 60;
+var time_left = 0;
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
 var intervalID;
+
 
 $(document).ready(function () {
     intervalID = setInterval(timer, 1000);
-   
+    time_left = parseInt($("#timer").text());
 
+    $("#quiz").on('submit', function (event) {
+        event.preventDefault();
+        console.log("HERE");
+    });
 });
 
 
@@ -19,6 +26,28 @@ function timer() {
     }
 }
 
+
 function stop() {
     clearInterval(intervalID);
+    get_score();
+    $("#answers").css("z-index", "1");
+    $("#answers").css("opacity", "1");
+}
+
+function get_score() {
+    for (var i = 1; i <= 6; i++) {
+        var selValue = $('input[name=q' + i + ']:checked').val();
+
+        if (selValue == 1) {
+            correct++;
+        } else if (selValue == 0) {
+            wrong++;
+        } else {
+            unanswered++;
+        }
+    }
+    console.log(correct, wrong, unanswered);
+    $("#RA").text(correct);
+    $("#WA").text(wrong);
+    $("#UnA").text(unanswered);
 }
